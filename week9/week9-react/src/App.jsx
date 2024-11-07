811ee36cfabeb9cbef2c05a9068ad4fcd7a5de94
BROCODES2024 <chetanreddyk394@gmail.com>
2024-11-08 00:00:12 +0530

    changes

--- week9/week9-react/src/App.jsx
+++ week9/week9-react/src/App.jsx
@@ -1,26 +1,31 @@
-import { useState, useEffect } from "react";
-
+import { useEffect, useState } from "react";
 function App() {
   return (
     <div>
       <Counter></Counter>
+      {/* Counter is a function, and these functions need to be rendered inside the App */}
     </div>
   );
 }
-
+//mounting(running for the 1st time),re-rendering,unmounting
 function Counter() {
+  // converting to state variable
   const [count, setCount] = useState(0);
-  console.log("counter");
+
+  //no matter how many renders happen setinterval runs only once hences flashes in the clock stops
   useEffect(function () {
-    setInterval(function () {
-      setCount((count) => count + 1);
+    setInterval(() => {
+      // setCount((count) => count + 1); this is equivalent to below function
+      setCount(function (count) {
+        return count + 1;
+      });
     }, 1000);
-  }, []);
+  }, []); //[] this is dependency array
+
   return (
     <div>
       <h1>{count}</h1>
     </div>
   );
 }
-
 export default App;
