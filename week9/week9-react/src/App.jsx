d4557ebba9548667f9ee8107575e23cf910b9d6e
BROCODES2024 <chetanreddyk394@gmail.com>
2024-11-14 11:28:52 +0530

    added week 7

--- week9/week9-react/src/App.jsx
+++ week9/week9-react/src/App.jsx
@@ -1,26 +1,27 @@
+import React, { useState } from "react";
+
 function App() {
   return (
     <div>
-      <PostComponent></PostComponent>
+      <Toggle />
     </div>
   );
 }
-const style1 = {
-  width: 200,
-  backgroundColor: "white",
-  borderRadius: 10,
-  borderColor: "gray",
-};
-function PostComponent() {
+
+function Toggle() {
+  const [isVisible, setIsVisible] = useState(true);
+
+  function toggle() {
+    setIsVisible((isVisible) => !isVisible);
+  }
+
   return (
-    <div style={style1}>
-      <img
-        src={
-          "https://appx-wsb-gcp.akamai.net.in/subject/2023-01-17-0.17044360120951185.jpg"
-        }
-        style={{ width: 20, height: 20, borderRadius: 20 }}
-      />
+    <div>
+      <button onClick={toggle}>Toggle Message</button>
+      <br />
+      {isVisible ? "Hi" : null}
     </div>
   );
 }
+
 export default App;
