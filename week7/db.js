ec4990cd0598b92665d506b27f7e3889e874de4a
BROCODES2024 <chetanreddyk394@gmail.com>
2024-11-15 13:02:33 +0530

    changes

--- week7/db.js
+++ week7/db.js
@@ -1,20 +1,23 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const objectID = mongoose.objectID;
+
 const User = new Schema({
   name: String,
   email: { type: String, unique: true },
   password: String,
 });
-const todo = new Schema({
-  userID: objectID,
+
+const Todo = new Schema({
+  userID: { type: Schema.Types.ObjectId, ref: "users" },
   title: String,
   done: Boolean,
 });
+
 const Usermodel = mongoose.model("users", User);
-const todomodel = mongoose.model("todos", todo);
-//exporting since they arent in the same file
+const Todomodel = mongoose.model("todos", Todo);
+
+// Exporting models
 module.exports = {
   Usermodel: Usermodel,
-  todomodel: todomodel,
+  Todomodel: Todomodel,
 };
--- week7/index.js
+++ week7/index.js
@@ -1,45 +1,58 @@
 const express = require("express");
-const { Usermodel, todomodel } = require("./db");
+const { Usermodel, Todomodel } = require("./db");
 const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
 const app = express();
-app.use(express.json());
-const JWT_SECRET = "CHETAN@REDDY@394";
-// to use await name the function async
-app.post("/signup", async function (req, res) {
-  const email = req.body.email;
-  const password = req.body.password;
-  const name = req.body.name;
-  await Usermodel.insert({
-    email: email,
-    password: password,
-    name: name,
-  });
-  res.json({
-    message: "You are signed up",
-  });
-});
 
-app.post("/sigin", async function (req, res) {
-  const email = req.body.email;
-  const password = req.body.password;
-  const user = await Usermodel.findOne({
-    email: email,
-    password: password,
-  });
-  console.log(user);
-  if (user) {
-    const token = jwt.sign({
-      id: user._id.toString(),
-    });
-    res.json({
-      token: token,
-    });
-  } else {
-    res.status(403).json({
-      message: "Incorrect Credentials",
-    });
+mongoose.connect(
+  "mongodb+srv://chetanreddyk394:3O58Ur47vciHi2Ib@cluster0.k8wss.mongodb.net/todos"
+);
+
+app.use(express.json());
+
+const JWT_SECRET = "CHETAN@REDDY@394";
+
+// Signup endpoint
+app.post("/signup", async function (req, res) {
+  const { email, password, name } = req.body;
+
+  try {
+    await Usermodel.create({ email, password, name });
+    res.json({ message: "You are signed up" });
+  } catch (error) {
+    console.error("Error during signup:", error);
+    res.status(500).json({ message: "Error signing up user" });
   }
 });
-app.post("/todo", function () {});
-app.get("/todos", function () {});
-app.listen(3000);
+
+// Signin endpoint
+app.post("/signin", async function (req, res) {
+  const { email, password } = req.body;
+
+  try {
+    const user = await Usermodel.findOne({ email, password });
+
+    if (user) {
+      const token = jwt.sign({ id: user._id.toString() }, JWT_SECRET);
+      res.json({ token });
+    } else {
+      res.status(403).json({ message: "Incorrect Credentials" });
+    }
+  } catch (error) {
+    console.error("Error during signin:", error);
+    res.status(500).json({ message: "Error signing in user" });
+  }
+});
+
+// Placeholder routes for /todo and /todos
+app.post("/todo", function (req, res) {
+  // Todo route implementation
+  res.send("Todo creation not yet implemented");
+});
+
+app.get("/todos", function (req, res) {
+  // Get todos implementation
+  res.send("Todos retrieval not yet implemented");
+});
+
+app.listen(3000, () => console.log("Server running on port 3000"));
