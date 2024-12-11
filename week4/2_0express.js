478fd6ca7bb56ab1694a009fc6f14db9c5775327
BROCODES2024 <chetanreddyk394@gmail.com>
2024-12-11 16:17:46 +0530

    changes

--- week4/2_0express.js
+++ week4/2_0express.js
@@ -8,6 +8,10 @@ function sum(n) {
 app.get("/", function (req, res) {
   const n = req.query.n;
   const ans = sum(n);
-  res.send(`hi there your sum is ${ans}`);
+  res.send(`Hi there, your sum is ${ans}`);
 });
-app.listen(3000); //its now running and listens to changes occured and responds accordingly
+app.listen(3000, () => {
+  console.log("Server is running");
+}); //its now running and listens to changes occured and responds accordingly
+
+// function(req,res){} == (req,res)=>{}
